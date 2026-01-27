8fcd8a2507511a990cb960b8ff93676cdc271316
IsraelVivancoC <israel.vivanco.c@gmail.com>
2026-01-26 23:33:46 -0600

    script for jdk 17

--- scripts/use-java.17.bat
+++ scripts/use-java.17.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Switch to Java 17 for current terminal session
 
-set JAVA_HOME=C:\Program Files\Java\jdk17
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.xxx
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 echo Using Java:
